8f1ac9068f6cb234186f7c7087fd92d34fa7f75c
Shane Skikne <sskikne@Shane-Skikne.(none)>
2013-02-27 00:31:29 -0500

    login test complete

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,31 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' });
 };
+
+//test route for making views
+exports.login = function(req, res){
+  res.render('login', { title: 'Express' });
+};
+
+
+//test route for making views
+exports.gameexplorer = function(req, res){
+  res.render('gameexplorer', { title: 'Express' });
+};
+
+
+//test route for making views
+exports.gameview = function(req, res){
+  res.render('gameview', { title: 'Express' });
+};
+
+
+
+//test route for making views
+exports.signin = function(req, res){
+    console.log(req.body)
+    if (req.body.inputUsername == "fail"){
+        res.send("Password failed! Please try again")
+    }
+        res.render('gameview', { title: 'Express' });
+};

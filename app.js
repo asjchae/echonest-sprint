879de1c8ffda9349985921be17bb22d429592edc
achae <achae@ubuntu.ubuntu-domain>
2013-02-27 00:09:24 -0500

    Typos are dumb

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.get('/gameview', routes.gameview);
 // POST requests
 
 // Tests
-app.get('/echotest', echcotest.index);
+app.get('/echotest', echotest.index);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));

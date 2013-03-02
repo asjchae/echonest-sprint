dab1ea772be6ee69f409bc12cfd2e67613ee6530
achae <achae@ubuntu.ubuntu-domain>
2013-03-01 22:49:27 -0500

    Changing gameplay

--- routes/gameplay.js
+++ routes/gameplay.js
@@ -4,17 +4,52 @@ var echojs = require('echojs')
 	, mongoose = require('mongoose')
 	, async = require('async');
 
+// NOTE: don't delete card until after dealer chooses so we know who won
 
-// app.get('/newround', gameplay.newround); // start a new round
-// app.get('/dealerscreen', gameplay.dealerscreen); // the screen the dealer sees to pick
-// app.get('/dealerwait', gameplay.dealerwait); // waiting screen for the dealer while players pick
-// app.get('/playerscreen', gameplay.playerscreen); // players can choose
-// app.get('/playerwait', gameplay.playerwait); // players wait for the dealer to choose
 // app.get('/roundfinish', gameplay.roundfinish);  // everyone sees the results
-
-// // note: don't delete card until after dealer chooses so we know who won
 
 // app.post('/start', gameplay.start);
 // app.post('/playersubmit', gameplay.playersubmit);
 // app.post('/dealersubmit', gameplay.dealersubmit);
 
+// Starts a new round.
+exports.newround = function(req, res) {
+
+};
+
+// The screen the dealer sees to pick.
+exports.dealerscreen = function(req, res) {
+
+};
+
+// The waiting screen for the dealer while players pick their submissions.
+exports.dealerwait = function(req, res) {
+
+};
+
+// Screen where players can choose which card to submit.
+exports.playerscreen = function(req, res) {
+
+};
+
+// Screen that players see while waiting for the dealer to choose; they can see all the submitted cards.
+exports.playerwait = function(req, res) {
+
+};
+
+
+exports.roundfinish = function(req, res) {
+
+};
+
+exports.start = function(req, res) {
+
+};
+
+exports.playersubmit = function(req, res) {
+
+};
+
+exports.dealersubmit = function(req, res) {
+
+};

6dd288d9b0dcc339204485908d8e1f739ac3c8d6
Shane Skikne <sskikne@Shane-Skikne.(none)>
2013-03-01 22:49:05 -0500

    commented out gets and posts

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.configure('development', function(){
 
 // GET requests
 app.get('/', routes.index);
-app.get('/users', user.list);
+// app.get('/users', user.list);
 app.get('/login', routes.login);
 app.get('/gameexplorer', routes.gameexplorer);
 app.get('/gameview', routes.gameview);
@@ -53,18 +53,18 @@ app.get('/themecards', admin.themecards);
 
 // Game Play
 
-app.get('/newround', gameplay.newround); // start a new round
-app.get('/dealerscreen', gameplay.dealerscreen); // the screen the dealer sees to pick
-app.get('/dealerwait', gameplay.dealerwait); // waiting screen for the dealer while players pick
-app.get('/playerscreen', gameplay.playerscreen); // players can choose
-app.get('/playerwait', gameplay.playerwait); // players wait for the dealer to choose
-app.get('/roundfinish', gameplay.roundfinish);  // everyone sees the results
+// app.get('/newround', gameplay.newround); // start a new round
+// app.get('/dealerscreen', gameplay.dealerscreen); // the screen the dealer sees to pick
+// app.get('/dealerwait', gameplay.dealerwait); // waiting screen for the dealer while players pick
+// app.get('/playerscreen', gameplay.playerscreen); // players can choose
+// app.get('/playerwait', gameplay.playerwait); // players wait for the dealer to choose
+// app.get('/roundfinish', gameplay.roundfinish);  // everyone sees the results
 
 // note: don't delete card until after dealer chooses so we know who won
 
-app.post('/start', gameplay.start);
-app.post('/playersubmit', gameplay.playersubmit);
-app.post('/dealersubmit', gameplay.dealersubmit);
+// app.post('/start', gameplay.start);
+// app.post('/playersubmit', gameplay.playersubmit);
+// app.post('/dealersubmit', gameplay.dealersubmit);
 
 
 

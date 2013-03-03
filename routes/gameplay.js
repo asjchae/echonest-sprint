cafa2e3bd6944bf80f5c689426de7ead9cdeda73
achae <achae@ubuntu.ubuntu-domain>
2013-03-02 23:58:29 -0500

    Working in gameplay.js, running into callback issues

--- routes/gameplay.js
+++ routes/gameplay.js
@@ -25,13 +25,27 @@ exports.dealerwait = function(req, res) {
 
 // Screen where players can choose which card to submit.
 exports.playerscreen = function(req, res) {
-    res.render('gameview', {title:'Express',theme:"happy", songs:[
-        {name:"Beauty And A Beat", artist:"Justin Bieber", id:"t17846091"},
-        {name:"Call Me Maybe", artist:"Carly Rae Jepsen", id:"t15832423"},
-        {name:"Ho Hey", artist:"The Lumineers", id:"t15999172  "},
-        {name:"It's Time", artist:"Imagine Dragons", id:"t15652628"},
-        {name:"Lights", artist:"Ellie Goulding", id:"t5482177"},
-        {name:"Little Talks", artist:"Of Monsters and Men", id:"t16498218"} ]})
+	// console.log(req.session.user);
+	var songs = [];
+	for (i=0; i<6; i++) {
+		SongCard.findOne({}).exec(function (err, response) {
+			if (err) {
+				console.log("Error getting song card", err);
+			} else {
+				console.log(response);
+				songs.push(response);
+			}
+		});
+	}
+
+	console.log(songs);
+    // res.render('gameview', {title:'Express',theme:"happy", songs:[
+    //     {name:"Beauty And A Beat", artist:"Justin Bieber", id:"t17846091"},
+    //     {name:"Call Me Maybe", artist:"Carly Rae Jepsen", id:"t15832423"},
+    //     {name:"Ho Hey", artist:"The Lumineers", id:"t15999172  "},
+    //     {name:"It's Time", artist:"Imagine Dragons", id:"t15652628"},
+    //     {name:"Lights", artist:"Ellie Goulding", id:"t5482177"},
+    //     {name:"Little Talks", artist:"Of Monsters and Men", id:"t16498218"} ]});
 };
 
 // Screen that players see while waiting for the dealer to choose; they can see all the submitted cards.
